3354720c617090acf94e037edec1ed4323f3eea8
wuqiao <wuqiao@xbwl.tech>
2025-08-06 17:52:58 +0800

    feat: 补充video-generator seo sections

---
+++ src/components/Seo/FreeButton.tsx
@@ -0,0 +1,14 @@
+const FreeButton = ({ href }: { href: string }) => {
+  return (
+    <a
+      href={href}
+      className="h-[48px] px-[24px] py-[16px] bg-[#6366F1] rounded-[10px] shadow-lg flex flex-row items-center justify-center w-fit cursor-pointer"
+    >
+      <p className="text-white text-[16px] font-bold cursor-pointer select-none">
+        Get Started for Free
+      </p>
+    </a>
+  );
+};
+
+export default FreeButton;
