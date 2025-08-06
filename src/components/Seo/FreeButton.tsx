e22cbe33acb6ad303d4b4ef95a9d306c29b48dc1
wuqiao <wuqiao@xbwl.tech>
2025-08-06 18:35:34 +0800

    fix: 更换按钮文案

--- src/components/Seo/FreeButton.tsx
+++ src/components/Seo/FreeButton.tsx
@@ -5,7 +5,7 @@ const FreeButton = ({ href }: { href: string }) => {
       className="h-[48px] px-[24px] py-[16px] bg-[#6366F1] rounded-[10px] shadow-lg flex flex-row items-center justify-center w-fit cursor-pointer"
     >
       <p className="text-white text-[16px] font-bold cursor-pointer select-none">
-        Get Started for Free
+        Get Started Now
       </p>
     </a>
   );
